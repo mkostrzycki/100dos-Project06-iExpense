c30a4779b608688550f185f545a5fd10b7f87d2a
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2024-04-10 16:03:16 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by MaćKo on 10/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
